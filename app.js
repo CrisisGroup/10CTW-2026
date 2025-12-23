61056b9d4c79100f5f37a3d63b39a3286fd0ca24
Paul Franz <paulkfranz@gmail.com>
2025-12-23 11:04:55 -0500

    mapbox cache refresh

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = "pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAO
 const scenes = {
   "world-order": {
     container: "map-world-order",
-    style: "mapbox://styles/daltonwb/cmjhoupyi000w01s0d45gel46",
+    style: "mapbox://styles/daltonwb/cmjirz4lc00a701qqaoat3ohj",
     initialView: { center: [30.08953, 13.76454], zoom: 4.3, pitch: 0, bearing: 0 },
     layers: {
       "sudan-countries": "sudan-countries",
@@ -75,7 +75,7 @@ const scenes = {
 
   "scene-2": {
     container: "map-scene-2",
-    style: "mapbox://styles/daltonwb/cmjhoupyi000w01s0d45gel46",
+    style: "mapbox://styles/daltonwb/cmjirz4lc00a701qqaoat3ohj",
     initialView: { center: [-5.23010, 12.53354], zoom: 4.5, pitch: 0, bearing: 0 },
     layers: {
       "niger_label": "niger_label",
@@ -121,7 +121,7 @@ const scenes = {
 
   "scene-3": {
     container: "map-scene-3",
-    style: "mapbox://styles/daltonwb/cmjhoupyi000w01s0d45gel46",
+    style: "mapbox://styles/daltonwb/cmjirz4lc00a701qqaoat3ohj",
     initialView: { center: [67.83852, 31.68396], zoom: 5.1, pitch: 0, bearing: 0 },
     layers: {
       "arabian-sea": "arabian-sea",
